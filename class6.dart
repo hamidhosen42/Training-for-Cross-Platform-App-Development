c47dc3d7840a933e158965316ab0f2a4a59d3e29
Md.Hamid Hosen <68488154+hamidhosen42@users.noreply.github.com>
2021-09-14 18:59:47 +0600

    Create class6.dart

---
+++ class6.dart
@@ -0,0 +1,103 @@
+// import 'package:flutter/material.dart';
+
+// main(List<String>arg)
+// {
+//   runApp(MaterialApp(
+//     debugShowCheckedModeBanner: false,
+//     home:Text("Hamid Hosne") ,
+
+//   ));
+// }
+
+// void main(List<String> args) {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget{
+//   @override
+//   Widget build (BuildContext context){
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: Center(
+//           child: Text("Hamid Hosen"),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// void main(List<String> args) {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: Center(
+//           child: Text(
+//             "Ovy and Hamid Hosen",
+//             style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+//           ),
+//           // body: Text("Ovy and Hamid Hosen")
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// void main(List<String> args) {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: Center(child: Text("Ovy gadha & goru",style: TextStyle(fontSize: 50,fontWeight: FontWeight.bold),))
+//           // child:Column(
+//           //   children:<Widget> [
+
+//       ),
+//     );
+//   }
+// }
+
+// void main(List<String> args) {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//           appBar: AppBar(
+//             backgroundColor: Colors.red,
+//             title: Center(
+//               child: Text(
+//                 "HomePage",
+//                 style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+//               ),
+//             ),
+//             actions: <Widget>[
+//               IconButton(onPressed: () {}, icon: Icon(Icons.message))
+//             ],
+//             leading: Image.network(
+//                 "https://cdn.pixabay.com/photo/2021/08/23/18/37/tea-6568547_1280.jpg"),
+//           ),
+//           body: Text("Flutter",
+//               style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold))
+//           // child:Column(
+//           //   children:<Widget> [
+            
+//           ),
+//     );
+//   }
+// }
