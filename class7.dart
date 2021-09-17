c505577d7a84b0e298313ee9d68c2b0d9cb86abe
Md.Hamid Hosen <68488154+hamidhosen42@users.noreply.github.com>
2021-09-17 09:48:29 +0600

    Create class7.dart

---
+++ class7.dart
@@ -0,0 +1,122 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Container(
+//         child: Scaffold(
+//           appBar: AppBar(
+//             title: Text(
+//               "Flutter",
+//               style: TextStyle(
+//                 fontSize: 50,
+//                 color: Colors.red,
+//               ),
+//             ),
+//             centerTitle: true,
+//             backgroundColor: Colors.yellow,
+//             actions: [
+//               Icon(
+//                 Icons.add_a_photo,
+//                 color: Colors.blue,
+//                 size: 30,
+//               ),
+//               IconButton(
+//                 onPressed: () {
+//                   print("Hamid Hosen");
+//                 },
+//                 icon: Icon(Icons.home),
+//                 iconSize: 30,
+//                 color: Colors.blue,
+//               )
+//             ],
+//             leading: Icon(
+//               Icons.message,
+//               color: Colors.blue,
+//               size: 30,
+//             ),
+//           ),
+
+//           //body: Image.network("https://cdn.pixabay.com/photo/2021/08/03/14/07/river-6519572_1280.jpg"),
+          
+//           body: SingleChildScrollView(
+//             scrollDirection: Axis.vertical, //Axis.horizontal
+//             child: Column(
+//               children: [
+//                 Container(
+//                     height: 400,
+//                     width: 400,
+//                     child: Image.asset("Image/img1.jpg"),
+//                     color: Colors.blue,
+//                     ),
+//                 Image.asset("Image/img1.jpg"),
+//                 Image.network(
+//                     "https://cdn.pixabay.com/photo/2021/08/03/14/07/river-6519572_1280.jpg"),
+//                 Image.network(
+//                     "https://cdn.pixabay.com/photo/2021/08/19/12/53/bremen-6557996_1280.jpg"),
+//                 Image.network(
+//                     "https://images.unsplash.com/photo-1575089976121-8ed7b2a54265?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"),
+//               ],
+//             ),
+//           ),
+
+//           ////////////////////////>>>>>>>>>>>>>>>>>>>>>>>>....................???/////////////////
+
+//           // body: Stack(
+//           //   children: [
+//           //     Image.network(
+//           //         "https://cdn.pixabay.com/photo/2021/08/03/14/07/river-6519572_1280.jpg"),
+//           //     Image.network(
+//           //         "https://cdn.pixabay.com/photo/2021/08/03/14/07/river-6519572_1280.jpg"),
+//           //     Image.network(
+//           //         "https://cdn.pixabay.com/photo/2021/08/19/12/53/bremen-6557996_1280.jpg"),
+//           //     Image.network(
+//           //         "https://images.unsplash.com/photo-1575089976121-8ed7b2a54265?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"),
+//           //     Center(
+//           //         child: Text(
+//           //       "ovy islam",
+//           //       style: TextStyle(
+//           //         fontSize: 50,
+//           //         fontWeight: FontWeight.bold,
+//           //         color: Colors.red[1000],
+//           //         letterSpacing: 12.4,
+//           //         height: 6.6,
+//           //         backgroundColor: Colors.red,
+//           //         wordSpacing: 10,
+//           //       ),
+//           //     )),
+//           //   ],
+//           // ),
+
+//           ////////////////////////...................................////////////////////////////////
+//           // body: Column(
+//           //   children: [
+//           //     Image.network("https://cdn.pixabay.com/photo/2021/08/03/14/07/river-6519572_1280.jpg"),
+//           //     Center(
+//           //         child: Text(
+//           //       "ovy islam",
+//           //       style: TextStyle(
+//           //         fontSize: 50,
+//           //         fontWeight: FontWeight.bold,
+//           //         color: Colors.red[1000],
+//           //         letterSpacing: 12.4,
+//           //         height: 6.6,
+//           //         backgroundColor: Colors.red,
+//           //         wordSpacing: 10,
+//           //       ),
+//           //     )),
+//           //   ],
+//           // ),
+//           // backgroundColor: Colors.green,
+//           /////////////////////////>..........>.....................................///////////////////////////////
+//         ),
+//       ),
+//     );
+//   }
+// }
