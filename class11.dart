43f964f4d2d3437c00e30a3fd37339876f501665
Md.Hamid Hosen <68488154+hamidhosen42@users.noreply.github.com>
2021-09-28 21:13:52 +0600

    Create class11.dart

---
+++ class11.dart
@@ -0,0 +1,164 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+
+//       home: Scaffold(
+//         appBar: AppBar(),
+//         body: Center(
+//           child: Card(
+//             child: Container(
+//               height: 200,
+//               width: 400,
+//               child: Column(
+//                 children: [
+//                   Text("Name : Hamid Hosen"),
+//                   Row(
+//                     children: [
+//                       Icon(Icons.location_on),
+//                       Text("Address : Cheragi Pahar,Chittagong")
+//                     ],
+//                   ),
+//                   Row(
+//                     children: [Icon(Icons.phone), Text("Phone : +9678392")],
+//                   ),
+//                   Row(
+//                     children: [
+//                       Icon(Icons.email),
+//                       Text("Email : hamidgjks@gmail.com")
+//                     ],
+//                   ),
+//                   Row(
+//                     children: [Icon(Icons.web),
+//                      Text("Web")
+//                     ],
+//                   ),
+//                   Row(
+//                     children: [Icon(Icons.favorite_outlined),
+//                      Text("Hobbies"),
+//                      Card(
+//                        elevation: 12,
+//                        child: Padding(
+//                          padding: const EdgeInsets.all(2.0),
+//                          child: Text("Slepping"),
+//                        ),
+//                      ),
+//                      Card(
+//                        elevation: 12,
+//                        child: Padding(
+//                          padding: const EdgeInsets.all(2.0),
+//                          child: Text("Music"),
+//                        ),
+//                      ),
+//                      Card(
+//                        elevation: 12,
+//                        child: Padding(
+//                          padding: const EdgeInsets.all(2.0),
+//                          child: Text("Travelling"),
+//                        ),
+//                      ),
+//                     ],
+//                   ),
+//                   Container(
+//                     height: 60,
+//                     child: Row(
+//                       mainAxisAlignment: MainAxisAlignment.end,
+//                       crossAxisAlignment: CrossAxisAlignment.end,
+//                       children: [
+//                         Icon(Icons.facebook),
+//                         Text("FaceBook"),
+//                       ],
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+
+//       // home: Scaffold(
+//       //   backgroundColor: Colors.pink[100],
+//       //   body: Container(
+//       //     height: 700,
+//       //     width: 180,
+//       //     color: Colors.red,
+//       //     child: Row(
+//       //       mainAxisAlignment: MainAxisAlignment.end,
+//       //       children: [
+//       //        Column(
+//       //          mainAxisAlignment: MainAxisAlignment.center,
+//       //           children: [
+//       //             Container(
+//       //               height: 50,
+//       //               width: 50,
+//       //               color: Colors.blue,
+//       //             ),
+//       //             Container(
+//       //               height: 50,
+//       //               width: 50,
+//       //               color: Colors.orange,
+//       //             ),
+//       //             Container(
+//       //               height: 50,
+//       //               width: 50,
+//       //               color: Colors.green,
+//       //             ),
+//       //           ],
+//       //         ),
+//       //       ],
+//       //     ),
+//       //   ),
+//       // ),
+//     );
+//   }
+// }
+
+
+// // import 'package:flutter/material.dart';
+
+// // void main() {
+// //   runApp(MyApp());
+// // }
+
+// // class MyApp extends StatelessWidget {
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return MaterialApp(
+// //       debugShowCheckedModeBanner: false,
+// //       home: Scaffold(
+// //         appBar: AppBar(
+// //           title: Center(
+// //               child: Text(
+// //             "Home Page",
+// //             style: TextStyle(
+// //                 color: Colors.redAccent,
+// //                 fontFamily: "Ephesis",
+// //                 fontSize: 40,
+// //                 fontWeight: FontWeight.bold),
+// //           )),
+// //         ),
+// //         body: Center(
+// //           child: Padding(
+// //             padding: const EdgeInsets.all(20),
+// //             child: Text(
+// //               "Hamid Hosen",
+// //               style: TextStyle(
+// //                   color: Colors.blue,
+// //                   fontFamily: "Ephesis",
+// //                   fontSize: 50,
+// //                   fontWeight: FontWeight.bold),
+// //             ),
+// //           ),
+// //         ),
+// //       ),
+// //     );
+// //   }
+// // }
