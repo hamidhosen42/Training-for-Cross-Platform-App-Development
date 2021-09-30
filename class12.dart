cf1a882d01c9307629b4c818a910f0f6ed3c2d6e
Md.Hamid Hosen <68488154+hamidhosen42@users.noreply.github.com>
2021-09-30 19:37:55 +0600

    Create class12.dart

---
+++ class12.dart
@@ -0,0 +1,106 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MaterialApp(
+//     debugShowCheckedModeBanner: false,
+//     title: 'Flutter Demo',
+//     theme: ThemeData(
+//       primarySwatch: Colors.blue,
+//     ),
+//     home: MyApp(),
+//   ));
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.blue[100],
+//       appBar: AppBar(
+//         title: Center(
+//             child: Text("Lubaba Sultana",
+//                 style: TextStyle(
+//                   fontWeight: FontWeight.bold,
+//                   fontSize: 28,
+//                 ))),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//           onPressed: () {
+//             for (int i = 1; i <= 10; i++) {
+//               print("${i} Click");
+//             }
+//           },
+//           //child: Text("Add"),
+//           backgroundColor: Colors.orange,
+//           foregroundColor: Colors.white,
+//           elevation: 24,
+//           hoverElevation: 30,
+//           hoverColor: Colors.pink,
+//           splashColor: Colors.red,
+//           tooltip: 'This is Hamid vai',
+//           child: CircleAvatar(
+//             radius: 30,
+//             backgroundImage: NetworkImage(
+//                 "https://cdn.pixabay.com/photo/2021/06/24/12/35/sunset-6361127_1280.jpg"),
+//           )),
+
+//           body: Center(child:Container(
+            
+//             child: ClipOval(
+              
+//               child: Image.network("https://i0.wp.com/i.giphy.com/media/Z9WQLSrsQKH3uBbiXq/giphy.gif"),
+//             ),
+//           ),),
+
+// /////////////////////////////another code //////////////////////////////
+
+//       // body: Center(
+//       //   child: CircleAvatar(
+//       //     child: Text(
+//       //       'Hamid',
+//       //       style: TextStyle(
+//       //           fontSize: 35,
+//       //           fontFamily: 'Ephesis',
+//       //           color: Colors.red,
+//       //           fontWeight: FontWeight.bold),
+//       //     ),
+//       //     radius: 70,
+//       //     backgroundColor: Colors.transparent,
+//       //     foregroundColor: Colors.transparent,
+//       //     backgroundImage: NetworkImage(
+//       //         'https://upload.wikimedia.org/wikipedia/commons/1/18/Trefoil_Surface_Animated.gif'),
+//       //   ),
+//       // ),
+
+// /////////////////////////////another code //////////////////////////////
+
+
+//       // // body: Center(
+//       // //   child: Column(
+//       // //     //mainAxisAlignment: MainAxisAlignment.start,
+//       // //     crossAxisAlignment: CrossAxisAlignment.start,
+//       // //     children: [
+//       // //       Card(
+//       // //         child: (Image.network(
+//       // //             "https://cdn.pixabay.com/photo/2021/06/24/12/35/sunset-6361127_1280.jpg")),
+//       // //       ),
+//       // //       Card(
+//       // //         child: Container(
+//       // //           height: 200,
+//       // //           width: 200,
+//       // //           color: Colors.red,
+//       // //         ),
+//       // //       ),
+//       // //       Card(
+//       // //         child: Container(
+//       // //           height: 200,
+//       // //           width: 200,
+//       // //           color: Colors.yellow,
+//       // //         ),
+//       // //       )
+//       // //     ],
+//       // //   ),
+//       // // ),
+//     );
+//   }
+// }
