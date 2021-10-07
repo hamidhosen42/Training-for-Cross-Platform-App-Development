aa9df81c53cffd53e7b790554fe32ede66cdd0c9
Md.Hamid Hosen <68488154+hamidhosen42@users.noreply.github.com>
2021-10-07 10:50:26 +0600

    Add files via upload

---
+++ class14/lib/home_page.dart
@@ -0,0 +1,46 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import './www.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CircleAvatar(
+          backgroundColor: Colors.pinkAccent,
+          backgroundImage: NetworkImage("https://wallpaperaccess.com/full/1781532.jpg",),
+          radius: 100,
+          child: Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>PaggeTwo()));
+              },
+              style: ButtonStyle(
+            
+                splashFactory: InkRipple.splashFactory,
+
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20),),),
+                ),
+                shadowColor: MaterialStateProperty.all(Colors.black),
+                backgroundColor: MaterialStateProperty.all(Colors.amber.withOpacity(.2)),
+                elevation: MaterialStateProperty.all(30),
+                foregroundColor: MaterialStateProperty.all(Colors.black),
+              ),
+              child: Text(
+                "Click me!",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
