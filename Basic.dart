e1409a08cc30bd508a17d5f23b41cd06bdfea6f5
Md.Hamid Hosen <68488154+hamidhosen42@users.noreply.github.com>
2021-10-10 20:37:44 +0600

    Create Basic.dart

---
+++ Basic.dart
@@ -0,0 +1,38 @@
+// void main(){
+//   addition(int a,int b){
+//     return a+b;
+//   }
+
+//   print(addition(20, 10));
+// }
+
+
+// void main(){
+//   for(int i=1;i<=5;i++){
+//     print(i);
+//   }
+// }
+
+
+// void main(List<String> args) {
+//   var a=19;
+//   var b=10;
+//   if(a>=b){
+//     print("True");
+//   }else{
+//     print("False");
+//   }
+// }
+
+
+// void main(){
+//   var a=19;
+//   var b=15;//var=data type convarted
+//   var c=a+b;
+//   print(c);
+// }
+
+
+// void main(){
+//   print("Hamid Hosen");
+// }
